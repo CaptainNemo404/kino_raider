700a5555a75bbb25ad280c699d0725ee0ccf3a9f
CaptainNemo404 <168180606+CaptainNemo404@users.noreply.github.com>
2024-04-26 13:22:40 +0300

    Add files via upload

---
+++ pages/mainPage.php
@@ -0,0 +1,30 @@
+<!Doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="../style/style.css">
+        <title>KinoRaider</title>
+    </head>
+    <body>
+        <div class="main col">
+            <header class="header">
+                <div class="content row">
+                    <div class="logo">
+                        Kino-Raider
+                    </div>
+                    <div>
+                        <form action="../scripts/search.php" method="POST">
+                            <button type="submit">
+                                <img src="../assets/image/svg/search.svg">
+                            </button>
+                            <input name="search" class="search" type="text" placeholder="Поиск">
+                        </form>          
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </header>
+        </div>
+    </body>
+</html>
---
+++ scripts/search.php
@@ -0,0 +1,5 @@
+<?php
+if (isset($_POST["search"]))
+{
+    echo 'window.location.href = "../pages/MainPage.php";</script>';
+}   
