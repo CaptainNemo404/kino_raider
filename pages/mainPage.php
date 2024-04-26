46517c2de48a2c49fb1fe5d02006e63f02f684ab
CaptainNemo404 <168180606+CaptainNemo404@users.noreply.github.com>
2024-04-26 20:09:34 +0300

    Add files via upload

--- pages/mainPage.php
+++ pages/mainPage.php
@@ -1,30 +1,26 @@
-<!Doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="../style/style.css">
-        <title>KinoRaider</title>
-    </head>
-    <body>
-        <div class="main col">
-            <header class="header">
-                <div class="content row">
-                    <div class="logo">
-                        Kino-Raider
-                    </div>
-                    <div>
-                        <form action="../scripts/search.php" method="POST">
-                            <button type="submit">
-                                <img src="../assets/image/svg/search.svg">
-                            </button>
-                            <input name="search" class="search" type="text" placeholder="Поиск">
-                        </form>          
-                    </div>
-                    <div>
-
-                    </div>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="../style/normalize.css">
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body>
+<header class="header">
+        <div class="header__body">
+            <div class="body-header__container container">
+                <a href="../pages/mainPage.php" class="body-header__logo">Kino<p>Raider</p></a>
+                <div class="body-header__search">
+                    <form action="#" class="search-header">
+                        <button type="submit" class="search-header__btn"><img src="../assets/image/icons/search.svg" alt="Search" width="20"></button>
+                        <input autocomplete="off" type="text" name="search" placeholder="Поиск" class="search-header__input">
+                    </form>
                 </div>
-            </header>
+                <a href="login.html" class="header__user"><img src="../assets/image/icons/profile.svg"><p>Личный кабинет</p></a>
+            </div>
         </div>
-    </body>
+    </header>
+</body>
 </html>
